9d72ebfa61abb37d96b869b4be19e9c4d7d1a59a
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-09-09 22:38:39 +0530

    Prompt with Button

--- ballapp.py
+++ ballapp.py
@@ -6,7 +6,7 @@ temp=0.7
 
 model = st.selectbox(
     'GPT Model',
-    ('gpt-3.5-turbo', 'code-davinci-002 (Legacy)', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613', 'gpt-3.5-turbo-0301', 'text-davinci-003', 'text-davinci-002', 'babbage-002', 'davinci-002', 'text-davinci-003', 'text-davinci-002', 'davinci, curie', 'babbage', 'ada'))
+    ('gpt-3.5-turbo', 'davinci', 'code-davinci-002 (Legacy)', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613', 'gpt-3.5-turbo-0301', 'text-davinci-003', 'text-davinci-002', 'babbage-002', 'davinci-002', 'text-davinci-003', 'text-davinci-002', 'davinci, curie', 'babbage', 'ada'))
 temp=st.slider('Temperature', 0.0, 1.0, 0.7)
 api_key=st.text_input("Enter API Key","qhOkd68l0oS7icaNaR8YT3BlbkFJUXbBLv8AqrX0a57T4ax6")
 openai.api_key=api_key
@@ -16,7 +16,8 @@ Use just these opensource dependencies from Maven.
 Write some Java methods to translate the Pacs.008 to MT103.""")
 
 def process():
-    response = openai.ChatCompletion.create(
+    #response = openai.ChatCompletion.create(
+    response = openai.Completion.create(
         model=model,
         messages=[
             {"role": "system", "content": system_prompt},
